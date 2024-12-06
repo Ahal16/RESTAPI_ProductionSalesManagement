4a212e31e56ab2becf2aeb0bfc5947a62f404193
Ahalya <ahalyagirish02@gmail.com>
2024-12-06 14:59:21 +0530

    updated

--- ProductSalesManagement/Controllers/ProductSalesController.cs
+++ ProductSalesManagement/Controllers/ProductSalesController.cs
@@ -265,55 +265,5 @@ namespace ProductSalesManagement.Controllers
             return Ok(ots);
         }
         #endregion
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //// GET: api/<ProductSalesController>
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //// GET api/<ProductSalesController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/<ProductSalesController>
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        //// PUT api/<ProductSalesController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/<ProductSalesController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
